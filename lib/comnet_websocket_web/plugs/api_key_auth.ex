fdf24edb871a364bf8624ae66fac0f634e92f0c8
Si Thu Hpyo <sandaraion@gmail.com>
2025-09-21 21:57:08 +0700

    Refactor ApiKeyAuth plug to retrieve API key from application environment dynamically instead of using a module attribute, enhancing flexibility in key management.

--- lib/comnet_websocket_web/plugs/api_key_auth.ex
+++ lib/comnet_websocket_web/plugs/api_key_auth.ex
@@ -1,14 +1,14 @@
 defmodule ComnetWebsocketWeb.Plugs.ApiKeyAuth do
   import Plug.Conn
 
-  @api_key Application.compile_env(:comnet_websocket, :api_key)
-
   def init(default), do: default
 
   def call(conn, _opts) do
+    api_key = Application.get_env(:comnet_websocket, :api_key)
+
     case get_req_header(conn, "x-api-key") do
       [key] ->
-        if @api_key != nil and key == @api_key do
+        if api_key != nil and key == api_key do
           conn
         else
           conn
