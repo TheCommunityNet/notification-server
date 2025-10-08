37a94c6841b9ed9fb27a06bd421277f0ff931efc
Si Thu Hpyo <sandaraion@gmail.com>
2025-10-08 19:29:17 +0700

    Configure ComnetWebsocket for production environment
    - Added configuration for ComnetWebsocketWeb.Endpoint to set the host from the PHX_HOST environment variable.
    - Disabled origin checking and enforced SSL redirection for enhanced security in production.

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,10 @@ import Config
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :comnet_websocket, ComnetWebsocketWeb.Endpoint,
+  url: [host: System.get_env("PHX_HOST")],
+  check_origin: false,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
