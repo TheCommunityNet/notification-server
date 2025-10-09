0fdc3a4af20451d479b6fbdaea179ce7567d6a54
Si Thu Hpyo <sandaraion@gmail.com>
2025-10-09 14:13:16 +0700

    Enhance NotificationSocket to include IP address handling in connection logic
    - Updated the connect function in NotificationSocket to retrieve and assign the device's IP address from connection information.
    - Modified the call to DeviceService.save_device_activity to include the IP address, improving tracking of device activities.
    - Introduced a private function to extract the IP address from the connection headers, ensuring better handling of incoming connection data.

--- lib/comnet_websocket_web/channels/notification_socket.ex
+++ lib/comnet_websocket_web/channels/notification_socket.ex
@@ -16,12 +16,12 @@ defmodule ComnetWebsocketWeb.NotificationSocket do
   def connect(%{"device_id" => device_id}, socket, connect_info) do
     socket = assign(socket, :device_id, device_id)
 
-    IO.inspect(connect_info, label: "connect_info")
+    ip_address = get_ip_address(connect_info)
 
     DeviceService.save_device(%{device_id: device_id})
 
     socket =
-      case DeviceService.save_device_activity(%{device_id: device_id}) do
+      case DeviceService.save_device_activity(%{device_id: device_id, ip_address: ip_address}) do
         {:ok, device_activity} ->
           assign(socket, :connection_id, device_activity.connection_id)
 
@@ -40,5 +40,24 @@ defmodule ComnetWebsocketWeb.NotificationSocket do
       nil -> nil
       user_id -> "user:#{user_id}"
     end
+  end
+
+  defp get_ip_address(%{x_headers: headers_list}) do
+    header = Enum.find(headers_list, fn {key, _val} -> key == "x-forwarded-for" end)
+
+    case header do
+      nil ->
+        nil
+
+      {_key, value} ->
+        value
+
+      _ ->
+        nil
+    end
+  end
+
+  defp get_ip_address(_) do
+    nil
   end
 end
